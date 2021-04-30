a8a0b55c271382d1be8bebc2f8c2aa94b2e598b1
shauvia <e.szalwinska@gmail.com>
2021-04-30 14:41:54 -0700

    configuring webpack finished

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
+    mode: 'production',
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
@@ -20,6 +20,15 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+          // Simulate the removal of files
+          dry: true,
+          // Write Logs to Console
+          verbose: true,
+          // Automatically remove all unused webpack assets on rebuild
+          cleanStaleWebpackAssets: true,
+          protectWebpackAssets: false
+  })
     ]
 }
